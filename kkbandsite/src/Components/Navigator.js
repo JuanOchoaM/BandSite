6fa86548729233766d2e3e1f602aa759b0c5aba4
tsemayeuga <tjo59814@uga.edu>
2023-11-16 19:38:33 -0500

    added route navigation

--- kkbandsite/src/Components/Navigator.js
+++ kkbandsite/src/Components/Navigator.js
@@ -1,27 +1,33 @@
-import React from 'react';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 import "./navbar.css"
 
 export default function Navigator() {
   return (
-    <div id="navbar">
-      <ul>
-    <li>
-      About
-    </li>
-    <li>
-      Music
-    </li>
-    <li>
-      Live Performances
-    </li>
-    <li>
-      Merch
-    </li>
-    <li></li>
-    <li id="login">Log in / Sign Up</li>
-    </ul>
-  </div>
+   <Router>
+       <Routes>
+         <Route path="/music" element={<Music />}/>
+         <Route path="/live-performances" element={<LivePerformances />}/>
+         <Route path="/" element={<Home />}/>
+       </Routes>
+   </Router>
   
   );
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function Music() {
+  return <h2>Music</h2>;
+}
+
+function LivePerformances() {
+  return <h2>Live Performances</h2>;
 }
 
