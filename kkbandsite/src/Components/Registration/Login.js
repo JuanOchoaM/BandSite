4f21a3ef1aab386d7dd87595b8be90f82041cf0a
tsemayeuga <tjo59814@uga.edu>
2023-11-18 10:08:11 -0500

    added sign up form validation and button on submit action

--- kkbandsite/src/Components/Registration/Login.js
+++ kkbandsite/src/Components/Registration/Login.js
@@ -7,11 +7,11 @@ export default function Login() {
 
     return (
         <div>
-            <form>
+            <form class="login-form"> 
                 <h2>Login</h2>
                 <input  type="text" value={email} onChange={(e) => setEmail(e.target.value)}/>
                 <input  type="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
-                <button type="submit">Login</button>
+                <button class="login-submit" type="submit">Login</button>
                 <a href="/signup">Don't have an account? Sign up here!</a>
             </form>
         </div>
