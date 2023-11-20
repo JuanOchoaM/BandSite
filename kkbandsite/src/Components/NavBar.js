5fffe79815e0f4958bf0d71e7fc4962c699fcdcf
Hunter Haskins <hhh14799@uga.edu>
2023-11-20 14:33:10 -0500

    Added a logo and title to the header nav bar

--- kkbandsite/src/Components/NavBar.js
+++ kkbandsite/src/Components/NavBar.js
@@ -5,20 +5,26 @@ import "./navbar.css"
 // Will
 export default function NavBar() {
         return (
-                    <div className="navbar">
-                        <NavLink to="/music" className="Navlink">
-                            Music
-                        </NavLink>
-                        <NavLink to="/live-performance" className="Navlink">
-                            Live Performances
-                        </NavLink>
-                        <NavLink to="/merch" className="Navlink">
-                            Merch
-                        </NavLink>
-                        <NavLink to="/signup" className="Navlink" id="login">
-                            Sign Up / Login
-                        </NavLink>
-                    </div>
+            <div className="navbar">
+               <div className="logo-title">
+               <img src="https://i.natgeofe.com/n/548467d8-c5f1-4551-9f58-6817a8d2c45e/NationalGeographic_2572187_3x4.jpg" alt="Karma Kat Logo" className="logo"/>
+                    <h1 className="title">Karma Kat</h1>
+               </div>
+                <div className="nav-links">
+                    <NavLink to="/music" className="Navlink">
+                        Music
+                    </NavLink>
+                    <NavLink to="/live-performance" className="Navlink">
+                        Live Performances
+                    </NavLink>
+                    <NavLink to="/merch" className="Navlink">
+                        Merch
+                    </NavLink>
+                    <NavLink to="/signup" className="Navlink" id="login">
+                        Sign Up / Login
+                    </NavLink>
+                </div>
+            </div>
         );
     };
      
