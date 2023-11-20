934a5ea51057fc1411213d730e6bd78e5034ed59
Hunter Haskins <hhh14799@uga.edu>
2023-11-20 17:34:24 -0500

    Additional formatting for card structure

--- kkbandsite/src/Components/Shop/AddCard.js
+++ kkbandsite/src/Components/Shop/AddCard.js
@@ -16,48 +16,48 @@ const AddCard = () => {
   
   return (
     <div className='card'>
-    <div className="addForm">
-      <h3>Add New Item</h3> {/* I've found that posting the alert is more reliable than using the console.log, cos the log clears on page reset */}
-      <form onSubmit={(e) => { alert("data object: " + JSON.stringify(data, null, 2)); e.target.reset(); } }>
-        <ul>
-          <li>
-        <label>Name</label>
-        <input 
-          onChange={(e) => setData({...data, Name:e.target.value})}
-          id="Name"
-          type="text"
-        />
-        </li>
-        <li>
-        <label>Price</label>
-        <input
-          onChange={(e) => setData({...data, Price:e.target.value})}
-          id="Price"
-          type="number"
-        />
-        </li>
-        <li>
-        <label>Link to image</label>
-         <input
-          onChange={(e) => setData({...data, Image:e.target.value})}
-          id="Image"
-          type="text"
-        />
-        </li>
-        <li>
-        <label>Description</label>
-         <input
-          onChange={(e) => setData({...data, Body:e.target.value})}
-          id="Body"
-          type="text"
-        />
-        </li>
-        <li>
-        <Button type="submit">Add User</Button>
-        </li>
-        </ul>
-      </form>
-    </div>
+      <div className="addForm">
+        <h3>Add New Item</h3> {/* I've found that posting the alert is more reliable than using the console.log, cos the log clears on page reset */}
+        <form onSubmit={(e) => { alert("data object: " + JSON.stringify(data, null, 2)); e.target.reset(); } }>
+          <ul className="form-information">
+            <li className="form-input">
+              <label className="add-name">Name</label>
+              <input 
+                onChange={(e) => setData({...data, Name:e.target.value})}
+                id="Name"
+                type="text"
+              />
+            </li>
+            <li className="form-input">
+              <label className="add-price">Price</label>
+              <input
+                onChange={(e) => setData({...data, Price:e.target.value})}
+                id="Price"
+                type="number"
+              />
+            </li>
+            <li className="form-input">
+              <label className="add-image">Link to image</label>
+              <input
+                onChange={(e) => setData({...data, Image:e.target.value})}
+                id="Image"
+                type="text"
+              />
+            </li>
+            <li className="form-input"> 
+              <label className="add-description">Description</label>
+              <input
+                onChange={(e) => setData({...data, Body:e.target.value})}
+                id="Body"
+                type="text"
+              />
+            </li>
+            <li>
+              <Button type="submit">Add User</Button>
+            </li>
+          </ul>
+        </form>
+      </div>
     </div>
   );
 }
