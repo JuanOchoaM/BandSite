db49a56fef43148ea4460ae7cf6f883efbcdde06
JuanOchoaM <95941336+JuanOchoaM@users.noreply.github.com>
2023-11-28 20:25:51 +0000

    Added backend and npm installed packages

--- kkbandsite/server/app.js
+++ kkbandsite/server/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 4200;
+const port = process.env.PORT || 9001;
+app.listen(port, () => console.log(`Server running on port
+${port}`));
 const mongoose = require('mongoose');
 const cors = require('cors');
 
